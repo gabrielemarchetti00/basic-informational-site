42b30ce52835533fdff514db406d192aa8fa156a
gabrielemarchetti00 <gabriele.marchetti26@gmail.com>
2023-06-05 16:36:07 +0200

    fix filename

--- index.js
+++ index.js
@@ -5,38 +5,25 @@ let fs = require('fs');
 http.createServer(function (req, res) {
   let q = url.parse(req.url, true);
   let filename = "." + q.pathname;
-  if(filename == ".") {
-    filename == "./index.html";
-    fs.readFile(filename, function(err, data) {
-      if(err) {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        return res.end("404 Not Found");
-      }
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      return res.end();
-    })
+  if (filename == "./") {
+    filename = "./index.html";
+  }
+  else if (filename == "./about") {
+    filename = "./about.html";
+  }
+  else if (filename == "./contact-me") {
+    filename = "./contact-me.html"
   }
   else {
-    fs.readFile(filename, function(err, data) {
-      if (err) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        filename = "./404.html";
-        fs.readFile(filename, function(err, data) {
-          if(err) {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            return res.end("404 Not Found");
-          }
-          res.writeHead(200, {'Content-Type': 'text/html'});
-          res.write(data);
-          return res.end();
-        })
-      }
-      else {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-      }
-    });
+    filename = "./404.html"
   }
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
 }).listen(8080);
